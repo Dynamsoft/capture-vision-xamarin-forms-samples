964a193a4b17942eb1ecfee4dce36eb12f5aadbe
yanshen6 <yanshen_6@163.com>
2018-10-19 17:41:51 +0800

    add xamarin form sample

---
+++ examples/DBRXFSample/DBRXFSample/DBRXFSample.iOS/PostMan.cs
@@ -0,0 +1,25 @@
+﻿using DBRXFSample.iOS;
+using DBRiOS;
+using Foundation;
+
+[assembly:Xamarin.Forms.Dependency(typeof(PostMan))]
+namespace DBRXFSample.iOS
+{
+    class PostMan : IPostMan
+    {
+        DynamsoftBarcodeReader reader = new DynamsoftBarcodeReader("t0068MgAAAEMKwCG/nGtAHejYbWgJH1sqDrUEhjbY2iIPP+rd//VnS2xWkcqSLMF3cxKetujwrYi4MxyyYl2qim4I1KKY3tk=");
+        TextResult[] results;
+        NSError error;
+        public string getResult()
+        {
+            string path = NSBundle.MainBundle.PathForResource("qr", "PNG");
+            UIKit.UIImage image = new UIKit.UIImage(path);
+            results = reader.DecodeImage(image, "", out error);
+            if(results != null && results.Length > 0)
+            {
+                return results[0].BarcodeText;
+            }
+            return "Barcode not found.";
+        }
+    }
+}
---
+++ examples/DBRXFSample/DBRXFSample/DBRXFSample/MainPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DBRXFSample
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool haveRead = false;
+        public MainPage()
+        {
+            InitializeComponent();
+            label.HorizontalTextAlignment = TextAlignment.Center;
+        }
+
+        void OnBtnReadClick(object sender, EventArgs args)
+        {
+            if (haveRead)
+            {
+                label.Text = "";
+                btn.Text = "Read";
+                haveRead = false;
+            }
+            else
+            {
+                var postMan = DependencyService.Get<IPostMan>();
+                label.Text = postMan.getResult();
+                btn.Text = "Clear";
+                haveRead = true;
+            }
+        }
+    }
+    
+    public interface IPostMan
+    {
+        string getResult();
+    }
+}
