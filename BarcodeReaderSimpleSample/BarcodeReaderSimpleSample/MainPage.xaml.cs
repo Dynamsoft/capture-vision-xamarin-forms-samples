a6953f42ff23ab7f6d4e2d426ce340098becf928
Dynamsoft-Henry <henry@dynamsoft.com>
2022-12-09 14:49:16 +0800

    Added some comments

--- BarcodeReaderSimpleSample/BarcodeReaderSimpleSample/MainPage.xaml.cs
+++ BarcodeReaderSimpleSample/BarcodeReaderSimpleSample/MainPage.xaml.cs
@@ -15,6 +15,10 @@ namespace BarcodeReaderSimpleSample
             InitializeComponent();
         }
 
+        // Go to the page of one-off barcode scanning.
+        // One-off scan: Scan barcode(s) from the video streaming and stop scanning after barcode results are output.
+        // You might use the barcode result(s) to fill-in forms or move to another page.
+        // In this sample, we go back to the front page and display the barcode result.
         async void oneoffscanButton_Clicked(object sender, EventArgs e)
         {
             var page = new OneoffScanPage();
@@ -24,7 +28,10 @@ namespace BarcodeReaderSimpleSample
             };
             await Navigation.PushAsync(page);
         }
-
+        
+        // Go to the page of continuous barcode scanning.
+        // Continuous scanning: Scan barcode(s) from the video streaming and output barcode results continuously.
+        // You can add configurations like duplicate result filter, result verification, control the barcode decoding interval, et al.
         async void OnCustomRendererButtonClicked(object sender, EventArgs e)
         {
             await Navigation.PushAsync(new CustomRendererPage());
